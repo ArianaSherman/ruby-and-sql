40bd39eb97d6715eba29f174715d58e2933bda2d
Ariana Sherman <ariana.sherman@kellogg.northwestern.edu>
2025-02-04 22:12:12 +0000

    labs

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -5,7 +5,7 @@
 
 # **************************
 # DON'T CHANGE OR MOVE
-Company.destroy_all
+Company.destroy_all #delete all rows before running this code and adding rows. prevent duplicates 
 # **************************
 
 # - Insert, read, update, and delete rows in companies table
@@ -13,15 +13,76 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts "There are #{Company.all.count} companies" #counts all lines in the company table 
+# aka SELECT COUNT(*) from Company in SQL 
+
 
 # 2. insert new rows in companies table
+new_company=Company.new # .new creates a new row, insert into model 
+#puts new_company.inspect
+new_company["name"]="Apple"
+new_company["city"]="Cupertino"
+new_company["state"]="CA"
+new_company["url"]="https://www.apple.com"
+#puts new_company.inspect
+
+new_company.save #save inserts these changes into the database. Converts from empty hash to an actual row 
+#puts "There are #{Company.all.count} companies" #counts all lines in the company table 
+
+new_company2=Company.new #don't have to do new variable, would overwrite, but cleaner to have a new variable when making the next row 
+new_company2["name"]="Amazon"
+new_company2["city"]="Seattle"
+new_company2["state"]="WA"
+#puts new_company.inspect
+
+new_company2.save #save inserts these changes into the database. Converts from empty hash to an actual row 
+#puts "There are #{Company.all.count} companies" #counts all lines in the company table 
+
+new_company3=Company.new #don't have to do new variable, would overwrite, but cleaner to have a new variable when making the next row 
+new_company3["name"]="Basecamp"
+new_company3["city"]="Chicago"
+new_company3["state"]="IL"
+new_company3["url"]="https://www.basecamp.com"
+#puts new_company.inspect
+
+new_company4=Company.new # .new creates a new row, insert into model 
+#puts new_company.inspect
+new_company4["name"]="Meta"
+new_company4["city"]="Menlo"
+new_company4["state"]="CA"
+new_company4["url"]="https://www.meta.com"
+#puts new_company.inspect
+
+new_company3.save #save inserts these changes into the database. Converts from empty hash to an actual row 
+
+new_company4.save #save inserts these changes into the database. Converts from empty hash to an actual row 
+puts "There are #{Company.all.count} companies" #counts all lines in the company table
 
 # 3. query companies table to find all row with California company
+puts "Cali Companies: #{Company.where({"state"=>"CA"}).count}"
+cali_companies=Company.where({"state"=>"CA"})
+puts cali_companies.inspect #active record relation, array 
+
 
 # 4. query companies table to find single row for Apple
+# apple=Company.where({"name"=>"Apple"})
+# puts apple.inspect #prints out an array 
+
+apple=Company.find_by({"name"=>"Apple"}) #find_by will only give you 1 result 
+puts apple.inspect #prints out an array 
 
 # 5. read a row's column value
+apple_url=apple["url"] #only show the apple URL 
+puts apple_url
+
 
 # 6. update a row's column value
+amazon=Company.find_by({"name"=>"Amazon"})
+amazon["url"]="https://www.amazon.com"
+amazon.save
+puts amazon.inspect
 
 # 7. delete a row
+meta=Company.find_by({"name"=>"Meta"})
+meta.destroy #deletes the row in the table 
+puts "Cali Companies: #{Company.where({"state"=>"CA"}).count}"
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -20,16 +20,47 @@ Salesperson.destroy_all
 # 1b. check out the model file
 
 # 2. insert 1-2 rows in salespeople table.
+new_salesperson=Salesperson.new
+new_salesperson["first_name"]="Ben"
+new_salesperson["last_name"]="Block"
+new_salesperson.save
+
+new_salesperson2=Salesperson.new
+new_salesperson2["first_name"]="Brian"
+new_salesperson2["last_name"]="Eng"
+new_salesperson2.save
+
+
 
 # 3. write code to display how many salespeople rows are in the database
-
+puts "There are #{Salesperson.all.count} salespeople"
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+ben=Salesperson.find_by({"first_name"=>"Ben","last_name"=>"Block"}) #typically, searching by first name isn't guaranteed. email would be most effective way to search for users when the database is more robuts 
+ben["email"]="bblock@gmail.com"
+ben.save
+# puts ben.inspect 
+
+brian=Salesperson.find_by({"first_name"=>"Brian"})
+brian["email"]="brian@aol.com"
+brian.save 
+# puts brian.inspect 
+
 
 # CHALLENGE:
-# 5. write code to display each salesperson's full name
+# 5. write code to display each salesperson's full name 
+salespeople=Salesperson.all 
+for person in salespeople 
+    first_name=person["first_name"]
+    last_name=person["last_name"]
+    name="#{first_name} #{last_name}"
+    puts name
+end 
+# for person in Salesperson -- this doesn't work because you need to get all rows first 
+#     puts "#{person["first_name"]} #{person["last_name"]}"
+# end
 
 # ---------------------------------
 # Salespeople: 2
